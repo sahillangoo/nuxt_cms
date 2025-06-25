3841a7087091f7857bce291982a7ef09f105bcf3
Sahil Langoo <40671195+sahillangoo@users.noreply.github.com>
2025-06-25 14:19:56 +0000

    initial commit 🚀

---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1 class="text-3xl font-bold text-purple-500 underline">
+    Hello world!
+  </h1>
+  <button class="btn btn-primary">Primary</button>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,28 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+export default defineNuxtConfig({
+  compatibilityDate: '2025-05-15',
+  devtools: {
+    enabled: true,
+
+    timeline: {
+      enabled: true
+    }
+  },
+  css: ['~/assets/css/main.css'],
+   vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+
+  modules: [
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/scripts',
+    '@nuxt/test-utils'
+  ]
+})
